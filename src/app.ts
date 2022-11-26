7df8cc5cd41422e7cf839aa26ba334b8887c42de
Arya <aryadytm12@gmail.com>
2022-11-26 13:45:21 +0700

    Initial Commit

---
+++ src/app.ts
@@ -0,0 +1,96 @@
+//@ts-check
+// Start Import
+import * as utils from './utils'
+import WAClient from './whatsapp_client'
+import express from 'express'
+import cors from 'cors';
+const helmet = require('helmet');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+// End Import
+
+const PORT = 40001
+
+const app = express();
+
+const client = new WAClient();
+
+// Create helper functions
+
+
+// adding Helmet to enhance your Rest API's security
+app.use(helmet());
+
+// use cors on all requests
+app.use(cors());
+
+// adding morgan to log HTTP requests
+app.use(morgan('combined'));
+
+// using bodyParser to parse JSON bodies into JS objects
+app.use(bodyParser.json());
+
+
+app.get('/', (req, res) => {
+  res.type('json');
+  res.send(`Good news, API is running! If you haven't linked your WA account, please go to ${client.getUrl()}/link_wa_account`);
+});
+
+
+app.get('/link_wa_account', (req, res) => {
+  
+  var resp = client.qrImageStr
+  return res.send(resp)
+})
+
+
+app.get('/reset_wa_account', (req, res) => {
+  
+  client.qrImageStr = "Unlinking your account... Please refresh this page after 1 minute."
+  res.redirect("/link_wa_account")
+  
+  new Promise(resolve => setTimeout(resolve, 8000)).then(() => {
+    client.onResetAccount()
+  })
+  
+}) 
+
+
+app.get('/send_wa_message/:targetNum/:message', (req, res) => {
+  
+  res.type('json');
+  
+  if (!client.isConnected) {
+    return res.status(400).send(`You have not linked any account. Please link it first.`)
+  }
+  
+  if (req.get('user-agent').toLowerCase().includes("whatsapp")) {
+    return res.status(400).send(`Not allowed`)
+  }
+  
+  const targetNum = utils.preprocessTarget(req.params.targetNum)
+  const message = utils.preprocessMessage(req.params.message)
+  
+  if (targetNum.length <= 5 || targetNum.length > 20) {
+    return res.status(400).send(`Gagal kirim pesan karena nomor target invalid.`)
+  }
+  
+  if (message.length > 1000) {
+    return res.status(400).send(`Gagal kirim pesan karena jumlah karakter melebihi 1000`)
+  }
+  
+  try {
+    client.client.sendMessage(targetNum, message).catch().finally()
+    return res.send(`Berhasil kirim pesan ke ${targetNum}\n-----------\nIsi Pesan: ${message}`)
+  } catch (err) {
+    return res.status(500).send(`Gagal kirim pesan: ${err.message}`)
+  }
+  
+});
+
+
+app.listen(PORT, () =>
+  console.log(`WA API listening on port ${PORT}!`),
+);
+
+
---
+++ src/whatsapp_client.ts
@@ -0,0 +1,153 @@
+//@ts-check
+import qrcode from 'qrcode-terminal'
+import wa from 'whatsapp-web.js'
+import dayjs from 'dayjs'
+import * as fs from 'fs'
+import * as utils from './utils'
+import { expectCt } from 'helmet'
+import localtunnel from 'localtunnel'
+
+
+type WAConfig = {
+  admin_number: string
+  public_domain_name: string
+}
+
+
+class WAClient {
+  client: wa.Client
+  isConnected: boolean
+  qrImageStr: string
+  config: WAConfig
+  tunnel: localtunnel.Tunnel
+  
+  constructor() {
+    this.initialize()
+    
+    // Use localtunnel to serve public
+    new Promise(resolve => setTimeout(resolve, 10000)).then(() => {
+      (async () => {
+        const tunnel = await localtunnel({
+          subdomain: this.config.public_domain_name,
+          port: 40001,
+        });
+        
+        console.log("Public URL has created.")
+        console.log(tunnel.url)
+        
+        tunnel.on('close', () => {
+          console.log("Public URL has closed.")
+        });
+        
+        this.tunnel = tunnel
+      })();
+    })
+  }
+  
+  initialize() {
+    console.log(`[${utils.getCurrentDateString()}] Initializing WA client...`)
+    
+    this.isConnected = false
+    this.qrImageStr = "Initializing API. Please keep refreshing this page!"
+    this.config = JSON.parse(fs.readFileSync('config.json'))
+    
+    this.client = new wa.Client({
+      authStrategy: new wa.LocalAuth({
+        clientId: `api-account`,
+        dataPath: `wwebjs_auth`
+      }),
+      puppeteer: {
+        executablePath: "/usr/bin/google-chrome-stable",
+        headless: true,
+        handleSIGINT: false,
+        args: [
+          "--no-sandbox",
+        ],
+      },
+    })
+
+    this.client.on('qr', qr => {
+      this.onClientQr(qr)
+    })
+
+    this.client.on('ready', () => {
+      this.onClientReady()
+    })
+    
+    this.client.on('disconnected', () => {
+      this.onClientDisconnected()
+    })
+
+    // Stop gracefully to save WA session
+    process.on("SIGINT", async () => {
+      console.log("\n");
+      console.log("(SIGINT) Shutting down...");
+      
+      await this.client.destroy();
+      
+      if (this.tunnel) {
+        this.tunnel.close()
+      }
+      
+      console.log("(SIGINT) Shut down success...");
+      process.exit(0);
+    })
+    
+    this.client.initialize()
+  }
+  
+  getUrl() {
+    return `https://${this.config.public_domain_name}.loca.lt`
+  }
+  
+  onClientReady() {
+    console.log(`[${utils.getCurrentDateString()}] Notifier client is ready!`)
+    
+    const clickHere = `<a href="/reset_wa_account">Click Here</a>`
+    
+    this.qrImageStr = `Account has been linked. Want to change to a different account? ${clickHere}.`;
+    this.client.sendMessage(utils.preprocessTarget(this.config.admin_number), `Sistem API WA berhasil dinyalakan dengan URL: ${this.getUrl()}`)
+    this.isConnected = true
+  }
+  
+  onClientQr(rawQr: string) {
+    qrcode.generate(rawQr, { small: true }, (_qrImageStr) => {
+      this.qrImageStr = (
+        "Scan this QR to link your WA account.\n" +
+        "Please refresh the page after link account.\n\n" +
+        "NOTE: This QR will expire in 30 seconds. Please refresh page if it happened. \n\n" + _qrImageStr
+      );
+      console.log(_qrImageStr)
+      console.log(`Please scan the QR code. You can also go to ${this.getUrl()}/link_wa_account to scan from web.`)
+    })
+  }
+  
+  onClientDisconnected() {
+    const clickHere = `<a href="/reset_wa_account">Click Here</a>`
+    this.isConnected = false
+    this.qrImageStr = `Account disconnected. Please ${clickHere} to relink your account.`
+  }
+  
+  onResetAccount() {
+    
+    if (!this.isConnected) {
+      return console.log("No client to reset");
+    }
+    
+    console.log("Unlink Account...")
+    
+    // this.client.removeAllListeners()
+    // NOTE: Expect this to reset along with Docker
+    this.client.logout()
+      .finally(() => {
+        console.log("Closed!")
+        this.tunnel.close()
+        process.exit(0)
+      })
+
+    this.isConnected = false
+  }
+  
+}
+
+export default WAClient
