e8aae52fbaacc0dd53481b065f40aad6a938d14a
Arya <aryadytm12@gmail.com>
2022-11-26 14:56:51 +0700

    Update install docker

--- install-docker.sh
+++ install-docker.sh
@@ -21,7 +21,5 @@ printf 'Waiting for Docker to start...\n\n'
 sleep 5
 
 # Docker Compose
-sudo wget --output-document=/usr/local/bin/docker-compose "https://github.com/docker/compose/releases/download/$(wget --quiet --output-document=- https://api.github.com/repos/docker/compose/releases/latest | grep --perl-regexp --only-matching '"tag_name": "\K.*?(?=")')/run.sh"
-sudo chmod +x /usr/local/bin/docker-compose
-sudo wget --output-document=/etc/bash_completion.d/docker-compose "https://raw.githubusercontent.com/docker/compose/$(docker-compose version --short)/contrib/completion/bash/docker-compose"
+sudo apt-get install docker-compose-plugin
 printf '\nDocker Compose installed successfully\n\n'
